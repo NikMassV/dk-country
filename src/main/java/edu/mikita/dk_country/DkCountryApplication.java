55b75da6e953705609011167b2b6d246ea6d2e06
slipk <slipknoshka777@gmail.com>
2024-10-11 20:00:28 +0300

    init

---
+++ src/main/java/edu/mikita/dk_country/DkCountryApplication.java
@@ -0,0 +1,13 @@
+package edu.mikita.dk_country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkCountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mikita/dk_country/DkCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mikita.dk_country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
